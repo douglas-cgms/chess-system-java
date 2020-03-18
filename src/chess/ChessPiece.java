3cbc3424c67e9133723eba3030cb5bc88c2be3c6
Douglas CGMS <douglasrodriguesposes@gmail.com>
2020-03-17 23:46:17 -0400

    Placing pieces on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,7 +4,8 @@ import boardgame.Board;
 import boardgame.Piece;
 
 public class ChessPiece extends Piece {
-	public ChessPiece(Board board) {
+	
+	public ChessPiece(Board board,Color color) {
 		super(board);
 		this.color = color;
 	}
