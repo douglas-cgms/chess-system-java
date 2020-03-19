7635928a91f238a3300770f3dd28f0054cc96182
Douglas CGMS <douglasrodriguesposes@gmail.com>
2020-03-18 23:21:30 -0400

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+	private static final long  serialVersionUId = 1l;
+	
+	public BoardException(String msg) {
+		super(msg);
+	}
+}
